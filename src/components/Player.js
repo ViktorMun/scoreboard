c8d42ac0cd65818ed99efcfe092aa960ad9e3e0a
Viktor Mun <v.mun@yahoo.com>
2018-04-30 11:45:28 +0200

    add proptypes

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,15 +1,32 @@
 import React, { PureComponent } from 'react'
 import './Player.css'
+import PropTypes from 'prop-types'
+
+export const playerShape = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired
+})
 
 export default class Player extends PureComponent {
+  static propTypes = {
+    ...playerShape.isRequired,
+  onChange: PropTypes.func.isRequired}
+
+  increasePlayerScore = () => {
+    const { id, score, onChange } = this.props
+    onChange(id, { score: score + 1 })
+  }
   render() {
+    // Getting name and score from the props, not id
     const { name, score } = this.props
+
 
     return (
       <li className="Player">
         <p className="name">{name}</p>
         <p className="score">{score}</p>
-      </li>
+        </li>
     )
   }
 }
